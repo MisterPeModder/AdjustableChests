44fcb548af0320051f1ec6bcfd35ebf4f1d42b09
MisterPeModder <yguaye44@gmail.com>
2017-09-10 01:53:49 +0200

    Initial commit

---
+++ src/main/java/misterpemodder/adjustablechests/ACRefs.java
@@ -0,0 +1,12 @@
+package misterpemodder.adjustablechests;
+
+public final class ACRefs {
+	
+	public static final String MOD_ID = "adjustablechests";
+	public static final String MOD_NAME = "Adjustable Chests";
+	
+	public static final String MOD_VERSION = "1.0.0";
+	public static final String ACCEPTED_HC_VERSIONS = "[1.0.2,)";
+	public static final String ACCEPTED_MC_VERSIONS = "[1.11.2]";
+	
+}
---
+++ src/main/java/misterpemodder/adjustablechests/AdjustableChests.java
@@ -0,0 +1,13 @@
+package misterpemodder.adjustablechests;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(
+		modid = ACRefs.MOD_ID,
+		name = ACRefs.MOD_NAME,
+		version = ACRefs.MOD_VERSION,
+		acceptedMinecraftVersions = ACRefs.ACCEPTED_HC_VERSIONS,
+		dependencies = "required-after:hc@" + ACRefs.ACCEPTED_MC_VERSIONS
+	)
+public class AdjustableChests {
+
+}
